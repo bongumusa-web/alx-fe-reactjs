9c53c2de2f31cf5d2ae9c207977c18bcdb673616
Bongumusa Gumede <bonggums@gmail.com>
2026-01-10 21:33:11 +0200

    alx-react-app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,11 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './Header.jsx'
+import Header from './components/Header.jsx'
 
-import MainContent from './MainContent.jsx'
+import MainContent from './components/MainContent.jsx'
 
-import Footer from './Footer.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
