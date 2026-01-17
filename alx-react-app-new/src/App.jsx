6873b1aa29e08b03b8958fcd42e98cf35b96cdd2
Bongumusa Gumede <bonggums@gmail.com>
2026-01-17 22:55:47 +0200

    Counter.jsx  and app.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -11,6 +11,8 @@ import Footer from './components/Footer.jsx'
 
 import UserProfile from './components/UserProfile.jsx'
 
+import Counter from './components/Counter.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -26,6 +28,9 @@ function App() {
         <MainContent />
 
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+
+        <Counter />
         
 
         <Footer />
