ff83d0277241f9d3ee3f0a21e67ebafa9c9152e6
Bongumusa Gumede <bonggums@gmail.com>
2026-01-17 23:28:44 +0200

    .\my-company\

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 import './App.css'
 
+import Home from './Home.jsx'
+import Contact from './Contact.jsx'
+import About from './About.jsx'
+import Services from './Services.jsx'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter> 
+    <div>
+      <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+        <ul style={{listStyle:"none", color:"blue", fontFamily:"monospace", float:"left", display:"flex", gap:"15px", fontSize:"20px"}}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
   )
 }
 
