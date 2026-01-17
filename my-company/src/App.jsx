12ec26f5fe56666a807cac3af77cb7840e5ef84e
Bongumusa Gumede <bonggums@gmail.com>
2026-01-17 23:33:44 +0200

    .\my-company\

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -6,21 +6,15 @@ import Home from './Home.jsx'
 import Contact from './Contact.jsx'
 import About from './About.jsx'
 import Services from './Services.jsx'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
 
   return (
     <BrowserRouter> 
     <div>
-      <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-        <ul style={{listStyle:"none", color:"blue", fontFamily:"monospace", float:"left", display:"flex", gap:"15px", fontSize:"20px"}}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-
+     
+      <Navbar />
 
 
       <Routes>
