956d5001cfd3941f4bcc3ec01e17ba732e6d878d
Bongumusa Gumede <bonggums@gmail.com>
2026-01-25 20:44:18 +0200

    recipe-sharing -app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,12 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div>
+      <AddRecipeForm />
+    </div>
+  );
+}
+
+export default App;
