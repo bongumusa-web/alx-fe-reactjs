744724f66c3fb07d92a68f5c5ab464f5a18d98bf
Bongumusa Gumede <bonggums@gmail.com>
2026-01-31 22:20:35 +0200

    github-user-search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1> GitHub User Search</h1>
+      <p>Search for GitHub users by username</p>
+      <p> Search for Github users by entering their username in the search box</p>
+      
+      
+    </div>
+  )
+}
+
+export default App
