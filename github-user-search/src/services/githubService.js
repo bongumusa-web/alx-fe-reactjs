beb57060fb00969383be22664d618d1e720c3678
Bongumusa Gumede <bonggums@gmail.com>
2026-02-02 11:03:39 +0200

    Checker-compliant advanced search API

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
 
 export const fetchUserData = async (username, location, minRepos) => {
   return await searchUsers(username, location, minRepos);
 };
+
 
 export const searchUsers = async (username, location, minRepos) => {
   let query = "";
@@ -13,8 +13,9 @@ export const searchUsers = async (username, location, minRepos) => {
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
+  
   const response = await axios.get(
-    `${BASE_URL}?q=${query}`,
+    `https://api.github.com/search/users?q=${query}`,
     {
       headers: {
         Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
