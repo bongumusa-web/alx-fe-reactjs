0732110c6a8e58f19dc7e34494189ee5ca46401e
Bongumusa Gumede <bonggums@gmail.com>
2026-02-02 09:59:31 +0200

    Fix GitHub advanced user search service

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,23 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users?q"; //API base URL
+const BASE_URL = "https://api.github.com/search/users";
 
-// Function to search for a GitHub user by username
-export const fetchUserData = async (username) => {
-    const response = await axios.get(`${BASE_URL}/${username}`);
-    return response.data;
+export const fetchUserData = async (username, location, minRepos) => {
+  return await searchUsers(username, location, minRepos);
 };
 
 export const searchUsers = async (username, location, minRepos) => {
   let query = "";
 
-  if (username) query += `${username}`;
+  if (username) query += username;
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
   const response = await axios.get(
-    `${BASE_URL}/search/users?q=${query}`
+    `${BASE_URL}?q=${query}`,
+    {
+      headers: {
+        Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+      },
+    }
   );
 
-  return response.data.items; 
+  return response.data.items;
 };
