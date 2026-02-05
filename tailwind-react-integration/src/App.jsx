a92e9a61095fbd3db520c788a3485a03715ababf
Bongumusa Gumede <bonggums@gmail.com>
2026-02-05 23:00:41 +0200

    .\tailwind-react-integration\

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,14 @@
+import UserProfile from './components/UserProfile';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  )
+}
+
+export default App
