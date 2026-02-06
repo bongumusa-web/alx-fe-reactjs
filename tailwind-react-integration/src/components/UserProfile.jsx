ccc5eb3532403ba1f870d21b258a1e07b7be35f1
Bongumusa Gumede <bonggums@gmail.com>
2026-02-06 22:38:01 +0200

    responsive for max-w-xs and sm:w-24 sm:h-24

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ import myprofile from "../assets/myprofile.png"
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src={myprofile} alt="User" className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto bg-red-200" />
+    <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+      <img src={myprofile} alt="User" className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto bg-red-200" />
       <h1 className=" text-lg md:text-xl text-blue-800 my-4 text-center">John Doe</h1>
       <p className="text-gray-600  text-sm md:text-base text-center">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
