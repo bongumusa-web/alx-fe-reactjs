7a84ef2aae871402db20ebc27e7c0bab5ede5a55
Bongumusa Gumede <bonggums@gmail.com>
2026-02-11 22:09:33 +0200

    recipe-sharing-platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="  bg-rose-950 p-6 w-[400px] m-auto my-2">
+      <h1 className="text-4xl text-blue-500 font-bold">
+      Tailwind Working
+    </h1>
+    </div>
+  )
+}
+
+export default App
