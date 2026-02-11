39414be3e6290bb5b93ecbc7e1eb12aa56029ccc
Bongumusa Gumede <bonggums@gmail.com>
2026-02-11 22:21:27 +0200

    .\recipe-sharing-platform\

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+ module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
