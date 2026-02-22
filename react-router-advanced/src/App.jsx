a446bff14aceb7f268338c9d86e731f63f8fe240
Bongumusa Gumede <bonggums@gmail.com>
2026-02-22 16:25:51 +0200

    updated App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,34 +1,32 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import ProfileDetails from "./pages/ProfileDetails";
 import ProfileSettings from "./pages/ProfileSettings";
-import BlogPost from "./pages/BlogPost.jsx";
-
-
+import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div>
+    <Router>
 
       {/* Navigation */}
       <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="/about">About</Link> |{" "}
         <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/blog/1">Blog Post</Link>
+        <Link to="/blog/1">Blog</Link>
       </nav>
 
       <Routes>
 
-        {/* Basic Routes */}
+        {/* Basic routes */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
 
-        {/* Protected Route */}
+        {/* Protected route */}
         <Route
           path="/profile"
           element={
@@ -37,16 +35,19 @@ function App() {
             </ProtectedRoute>
           }
         >
-          {/* Nested Routes */}
+
+          {/* Nested routes */}
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
+
         </Route>
 
-        {/* Dynamic Route */}
+        {/* Dynamic route */}
         <Route path="/blog/:id" element={<BlogPost />} />
 
       </Routes>
-    </div>
+
+    </Router>
   );
 }
 
